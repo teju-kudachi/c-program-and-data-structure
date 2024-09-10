7413e414df14a6a4e4626c9de66a670a25bb9f8f
teju-kudachi <1sv23ec041@shrideviengineering.org>
2024-09-10 20:06:58 +0900

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main(void) {
+int i=1,factorial=1,num;
+printf("enter a number");
+  scanf("%d",&num);
+  do
+    {
+      factorial=factorial*i;
+      i++;
+    }
+    while(i<=num);
+       printf("factorial of %d is %d",num,factorial);
+}
+
