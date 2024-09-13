af10aa9beec064e088c3e3ae4105ca54ba19f41d
teju-kudachi <1sv23ec041@shrideviengineering.org>
2024-09-13 19:50:39 +0900

    Add files via upload

---
+++ even odd.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void test(int num){
+  
+  printf("enter a number");
+  scanf("%d",&num);
+  if(num%2==0){
+    printf("even");
+  }
+  else{
+    printf("odd");
+  }
+}
+int main(){
+  int num, check;
+  test(num);
+  
+  
+}
+
+
+
+
+  
+    
+
