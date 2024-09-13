9824551669a567dd73c808ebbdb2c15452ad92fa
teju-kudachi <1sv23ec041@shrideviengineering.org>
2024-09-13 19:51:28 +0900

    Add files via upload

---
+++ pointers swap.c
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+ void swap(int *x,int *y)
+{
+  int temp = *x;
+  *x = *y;
+  *y = temp;
+}
+int main()
+{
+  int a=10,b=30;
+  printf("Before swapping a=%d,b=%d\n",a,b);
+  swap(&a,&b);
+  printf("a=%d b=%d\n",a,b);
+  return 0;
+}
+  
+
+   
+ 
