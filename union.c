259929cd1de2479315aecdbdb13b63ed6c656350
teju-kudachi <1sv23ec041@shrideviengineering.org>
2024-09-13 19:51:59 +0900

    Add files via upload

---
+++ union.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+union book {
+  int num;
+  char name[10];
+  float price;
+};
+int main() {
+  union book hp;
+  hp.num = 123;
+  hp.price = 1.234;
+  strcpy(hp.name, "Harry Potter");
+  printf("book number is %d\n", hp.num);
+  printf("book name is %s\n", hp.name);
+  printf("book price is %f\n", hp.price);
+  return 0;
+}
