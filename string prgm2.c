8186a3983ac324c181c25377b1a9f2f1f2c93661
teju-kudachi <1sv23ec041@shrideviengineering.org>
2024-09-13 19:49:38 +0900

    Add files via upload

---
+++ string prgm2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  int n;
+  char name[9];
+  printf("enter your name:");
+  scanf("%s",name);
+  printf("Hello %s,welcom to c programming",name);
+  
+  return 0;
+}
